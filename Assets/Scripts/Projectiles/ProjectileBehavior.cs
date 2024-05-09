de0edf99f8b5036980af4cc2d044264e4301b073
ytsiwT <charlievu@comcast.net>
2024-05-09 19:42:31 -0400

    Add sword projectile prototype

---
+++ Assets/Scripts/Projectiles/ProjectileBehavior.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehavior : MonoBehaviour
+{
+    public float Speed = 3.0f;
+
+    private void Update()
+    {
+        transform.position += -transform.right * Time.deltaTime * Speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
