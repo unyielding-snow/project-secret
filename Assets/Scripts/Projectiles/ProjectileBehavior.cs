d54a83cf0be8bf08314d8cc95a6346da2817e552
ytsiwT <charlievu@comcast.net>
2024-05-10 01:02:34 -0400

    Projectile damage base

--- Assets/Scripts/Projectiles/ProjectileBehavior.cs
+++ Assets/Scripts/Projectiles/ProjectileBehavior.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class ProjectileBehavior : MonoBehaviour
 {
     public float Speed = 3.0f;
+    public bool sourceEnemy = true;
+
+    // Only damage stuff on this layer 
+    public LayerMask damageLayers;
 
     private void Update()
     {
@@ -13,6 +17,31 @@ public class ProjectileBehavior : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        /*
+         *  Hardcoding object type for now based on name,
+         *  in the future check based on a specific tag?
+         *  (players, walls/border hitboxes, enemies)
+         */
+        if (sourceEnemy && collision.gameObject.name == "Player")
+        {
+            DoDamage(collision.gameObject);
+            Destroy(gameObject);
+        }
     }
+
+    private void DoDamage(GameObject target)
+    {
+        //Check if collision is included in layermask
+        if ((damageLayers.value & 1 << target.layer) != 0)
+        {
+            //Find health component on collided object
+            HealthSystem health = target.GetComponent<HealthSystem>();
+            if (health)
+            {
+                health.Damage(10, target);
+                Debug.Log("Damage Applied");
+            }
+        }
+    }
+
 }
