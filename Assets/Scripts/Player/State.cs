3eb606db776090bb44ffb2c23190bc418ba3a8b7
unyielding_snow <justin.tzehing.lo@gmail.com>
2024-05-16 03:19:22 -0400

    combat mockup
    - TODO: fix player attack direction

--- Assets/Scripts/Player/State.cs
+++ Assets/Scripts/Player/State.cs
@@ -9,10 +9,12 @@ public abstract class State : MonoBehaviour
     protected Rigidbody2D body;
     protected Animator animator;
     protected PlayerController player;
+    protected EnemyController enemy;
     protected float speed;
 
     // State Variables
     public bool isComplete {get; protected set;}
+
     protected float startTime;
     public float time => Time.time - startTime;  // => can't be set, so can be public
 
@@ -28,7 +30,19 @@ public abstract class State : MonoBehaviour
     {
         body = _body;
         animator = _animator;
+    }
 
-        //player = _player;
+    public void SetupPlayer(Rigidbody2D _body, Animator _animator, PlayerController _player)
+    {
+        body = _body;
+        animator = _animator;
+        player = _player;
+    }
+
+    public void SetupEnemy(Rigidbody2D _body, Animator _animator, EnemyController _enemy)
+    {
+        body = _body;
+        animator = _animator;
+        enemy = _enemy;
     }
 }
